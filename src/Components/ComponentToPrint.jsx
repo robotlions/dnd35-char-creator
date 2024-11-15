0b908a21dca9039edd8f40b4254c9eb575cbfdfe
Chad M <chadmusick@gmail.com>
2024-11-15 15:09:18 -0500

    cleans up js to clear errors, mostly

--- src/Components/ComponentToPrint.jsx
+++ src/Components/ComponentToPrint.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 import dndLogo from "../images/dndLogo.png";
 import * as RaceBonuses from "../Races/AbilBonuses";
 import * as CharInfo from './CharInfo';
